71ebccc218fa47d980c8255e089121c8e3d080ad
Zoe Song <zoe@Zoes-MacBook-Pro.local>
2022-10-19 11:24:05 -0500

    update scripts

--- bulletarm_baselines/fc_dqn/scripts/train.sh
+++ bulletarm_baselines/fc_dqn/scripts/train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for ((j=-8;j>=-10;j--));do
+for ((j=-4;j>=-7;j--));do
 {
-    CUDA_VISIBLE_DEVICES=2 python main.py --env=object_grasping --object_index=${j}
+    CUDA_VISIBLE_DEVICES=1 python main.py --env=object_grasping --object_index=${j}
     mv ../../../pos_info.txt ./pos_info_${j}.txt
 }
 done
